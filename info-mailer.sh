57081022098c38ab2c3fcaba6cf3537845e2f997
Arto Iijalainen <arto.iijalainen@gmail.com>
2012-06-15 21:51:55 +0300

    srv-info-mailer base commit.

---
+++ info-mailer.sh
@@ -0,0 +1,6 @@
+if [  $# -eq 0 ]
+then
+	echo "Please provide an email address to send to."
+	exit 1
+fi
+/usr/bin/sendEmail -f $(hostname)@server.com -t $1 -s mail.inet.fi -u "Server status" -o message-file=/etc/motd
