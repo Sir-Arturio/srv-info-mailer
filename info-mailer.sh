6c1b3c05ae4390239e756ee2002387525f2ffcba
Arto Iijalainen <arto.iijalainen@gmail.com>
2012-07-06 10:13:49 +0300

    Changed the hardcoded SMTP server value to a command line argument.

--- info-mailer.sh
+++ info-mailer.sh
@@ -1,6 +1,11 @@
 if [ ! "${1}" ]
 then
-	echo "Please provide an email address to send to."
+	echo "Please provide DESTINATION EMAIL."
 	exit 1
 fi
-/usr/bin/sendEmail -f $(hostname)@server.com -t $1 -s mail.inet.fi -u "Server status" -o message-file=/etc/motd
+if [ ! "${2}" ]
+then
+	echo "Please provide SMTP SERVER."
+	exit 1
+fi
+/usr/bin/sendEmail -f $(hostname)@server.com -t $1 -s $2 -u "Server status" -o message-file=/etc/motd
